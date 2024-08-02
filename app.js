208a2cecae0bebf6e839458bc0c1f55e891cb1da
geekylearner13 <155531479+geekylearner13@users.noreply.github.com>
2024-08-02 20:51:28 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+gsap.from("#Home h1",{
+    y:25,
+    duration:0.5,
+    delay:1,
+    opacity:0
+});
+gsap.from("#Home p",{
+    y:25,
+    duration:0.5,
+    delay:1.5,
+    opacity:0
+});
+gsap.from("#Home img",{
+    y:50,
+    duration:0.8,
+    delay:1,
+    scale:1.3,
+    opacity:0
+})
